4a0a430bc01ce7ee7b993e83a34ff15d380dae87
Kyler Deggs <deggskyler@icloud.com>
2020-12-29 16:17:22 -0600

    Initial commit

---
+++ src/main/java/com/kylerdeggs/javaconnected/JavaConnectedApplication.java
@@ -0,0 +1,13 @@
+package com.kylerdeggs.javaconnected;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaConnectedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaConnectedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kylerdeggs/javaconnected/JavaConnectedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kylerdeggs.javaconnected;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaConnectedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
